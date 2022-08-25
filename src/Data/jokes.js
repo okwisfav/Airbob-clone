cd90b330233c6e477cab8328d93401a85c1dcdbd
ofego favour <ofegofavour@gmail.com>
2022-08-25 09:43:42 +0100

    update

---
+++ src/Data/jokes.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        setup: " I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she open it."
+    },
+    {
+        setup: " I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she open it."
+    },
+    {
+        setup: " I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she open it."
+    },
+    {
+        setup: " I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she open it."
+    },
+    {
+        setup: " I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she open it."
+    },
+]
--- src/conponnet/Card.js
+++ src/conponnet/Card.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import hero from '../img/Group 77.png'
-import imgone from '../img/image 12.png';
-import star from '../img/Star 1.png'
-import '../Css/card.css';
-import Hero from './Hero';
+import jokesData from '../Data/jokes'
 const Card = () => {
+  const jokeElements = jokesData.mp(joke => {
+
+  })
   return (
-    <div className="card">
-        <Hero 
-         imgUrl={imgone}
-         imgicon={star}
-         spanone="5.0"
-         spantwo="(6)"
-         spanthree="USA" 
-         text="Life of a kids"
-         spanfour="from $136 / person"
-         />
+    <div>
+      
     </div>
   )
 }
